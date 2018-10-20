6e15b36fdebb8943e341d664e0199505435c4e71
Timo <timo@eyezag.de>
2018-10-20 17:35:29 +0200

    cleaned up code; added example json

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,41 +1,33 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import { Button, Menu, Input, Container, Form } from 'semantic-ui-react';
-import { getDataFromUrl } from './services/api';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Container, Form, Header, Image, Loader } from 'semantic-ui-react';
+import './App.css';
+import { getDataFromUrl } from './services/api';
+import PageMenu from './components/PageMenu';
+import { APP_NAME } from './config/constants';
 
-class PageMenu extends Component {
-  render() {
-    return (
-      <Menu stackable>
-        <Menu.Item>
-          {/* <img src='/images/logo.png' /> */}
-          Leadif.ai
-        </Menu.Item>
-      </Menu>
-    )
-  }
-}
 
 class EmailInput extends Component {
   state = { value: 'timo@usertimes.io' };
 
   static propTypes = {
-    onData: PropTypes.func
+    onData: PropTypes.func,
+    onLoading: PropTypes.func
   }
 
   static propTypes = {
-    onData: () => {}
+    onData: () => {},
+    onLoading: () => {}
   }
 
   onSubmit = async () => {
     const { value } = this.state;
-    const { onData } = this.props;
+    const { onData, onLoading } = this.props;
     if (!value) return;
     const domain = value.split('@').pop();
     if (!domain) return;
     this.setState({ loading: true });
+    onLoading();
     const { data } = await getDataFromUrl(domain);
     onData(data);
     this.setState({ loading: false });
@@ -51,7 +43,7 @@ class EmailInput extends Component {
     return (
       <Form>
         <Form.Input
-          label='Enter an email address'
+          // label='Enter an email address'
           action={{ icon: 'search', onClick: this.onSubmit }}
           fluid
           name='input'
@@ -60,28 +52,66 @@ class EmailInput extends Component {
           loading={loading}
           disabled={loading}
           onChange={this.onChange}
+          placeholder='someone@company.org'
         />
       </Form>
     );
   }
 }
 
+class Footer extends Component {
+  render() {
+    return (
+      <Container textAlign='center' style={{ marginTop: 80 }}>
+        &copy; leadif.ai – originated from the AI challenge by
+        <Image src='images/uniserve.png' centered />
+      </Container>
+    )
+  }
+}
+
 class App extends Component {
-  state = {};
+  state = { loading: false };
 
   onData = data => {
-    this.setState({ data });
+    this.setState({ data, loading: false });
+  }
+
+  onLoading = () => {
+    this.setState({ loading: false });
   }
 
   render() {
-    const { data } = this.state;
+    const { data, loading } = this.state;
 
     return (
       <div className="App">
         <PageMenu />
 
-        <Container>
-          <EmailInput onData={this.onData} />
+        <Container style={{ marginTop: 80, marginBottom: 80 }} text>
+          <Header as='h1' textAlign='center'>
+            This is { APP_NAME }
+            <Header.Subheader>
+              Lead Management with AI and Open Data
+            </Header.Subheader>
+          </Header>
+
+          <p style={{ marginTop: 40, marginBottom: 40, textAlign: 'center' }}>
+            Someone entered an email address on your website?<br />
+            Find out more about their company below.
+          </p>
+
+          <EmailInput
+            onData={this.onData}
+            onLoading={this.onLoading}
+          />
+
+          {
+            loading &&
+            <Loader active inline='centered' style={{ marginTop: 80 }} size='huge'>
+              Loading lead data...
+            </Loader>
+          }
 
           {
             data && <pre>{ JSON.stringify(data) }</pre>
@@ -89,6 +119,8 @@ class App extends Component {
 
 
         </Container>
+
+        <Footer />
 
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
