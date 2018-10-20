7de099a87f54a8ae1f661c5c0cf57da577b4f823
Timo <timo@eyezag.de>
2018-10-20 15:43:07 +0200

    needed to install CORS to allow cross origin AJAX calls

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get', methods=['GET'])
 def classify_url():
-    return jsonify({'result': request.args.get('text')})
+    return jsonify({'result': request.args.get('url')})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8090, use_reloader=False)
